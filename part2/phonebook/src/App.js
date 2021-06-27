e012de47151b6d745516d346771cb6331e3b51e1
aviral19 <bajpaiaviral19@gmail.com>
2021-06-27 17:56:06 +0530

    Project Phonebook completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './Components/filter'
 import Persons from './Components/persons'
 import PersonForm from './Components/personform'
@@ -6,14 +7,24 @@ import PersonForm from './Components/personform'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    // { name: 'Arto Hellas', number: '040-123456' },
+    // { name: 'Ada Lovelace', number: '39-44-5323523' },
+    // { name: 'Dan Abramov', number: '12-43-234345' },
+    // { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
   const [ filterName, setFilterName] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
